76fbf2d7a828fa29a8546a36be71d1609685877f
Gonzalo Lascar <44815702+gonzalas@users.noreply.github.com>
2020-04-12 21:05:06 -0300

    Introducción/Ejercicio-2

---
+++ src/ejercicio2.java
@@ -0,0 +1,24 @@
+
+/*
+Programa java que realice lo siguiente: declarar dos variables X e Y de tipo int, dos
+variables N y M de tipo double y asigna a cada una un valor. A continuación mostrar
+por pantalla una serie de operaciones entre ellas.
+
+ */
+
+public class ejercicio2 {
+
+    public static void main(String[]args){
+
+        int x = 5;
+        int y = 10;
+        double n = 10.123;
+        double m = 20.1;
+
+        System.out.println("x + y = " + (x + y));
+        System.out.println("n.m = " + (n*m));
+        System.out.println("m:y = " + (m/y));
+
+
+    }
+}
